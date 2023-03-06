c411fbc526a2a713d84316622672f145aa6ea284
Jane Charlott <janechar@ev-b1.kzn.21-school.ru>
2023-03-06 17:16:10 +0300

    D1T6

---
+++ src/quest3.sh
@@ -0,0 +1,7 @@
+mv door_management_fi door_management_files
+mkdir door_configuration
+mkdir door_map
+mkdir door_log
+cp door_*.conf ~/T01D01-0/src/door_management_files
+cp door_*.log ~/T01D01-0/src/door_management_files/door_logs
+cp door_map_1.1 ~/T01D01-0/src/door_management_files/door_map
---
+++ src/quest6.sh
@@ -0,0 +1,13 @@
+212  cat keygen.sh
+  213  cat unifier.sh
+  214  touch main.key
+  215  ls
+  216  rm main.key
+  217  ls
+  218  chmod 777 *
+  219  ./keygen.sh
+  220  ls -l
+  221  cd key
+  222  ls -l
+  223  rm -r file*
+  224  ls -l
